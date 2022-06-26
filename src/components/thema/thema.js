96ec5239f8c018853ee09a93b6681a1a8c8af8b1
[서울/2]김명진 <krocd923@gmail.com>
2022-06-26 20:42:51 +0900

    add thema

---
+++ src/components/thema/thema.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const FlexContent = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+export const componentsWrapper = styled.div`
+  box-sizing: border-box;
+  background-color: #ffc107;
+  width: 100%;
+  max-height: 90vh;
+  margin-top: 50px;
+  padding-bottom: 60px;
+`;
