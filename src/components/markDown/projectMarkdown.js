111139c014b4c4baaa85070fab52b16275e8efbc
[서울/2]김명진 <krocd923@gmail.com>
2022-06-27 01:49:05 +0900

    edit markdown

--- src/components/markDown/projectMarkdown.js
+++ src/components/markDown/projectMarkdown.js
@@ -1,13 +1,16 @@
 export const collectorArticle = {
-  title: `# 제목입니다`,
+  title: `# Collector’s`,
   article: `
-  ## 프로젝트 내용
-  * 내용입니다
-  * 내용2입니다
+  ## Collector's
+  * 쇼핑몰에 필요한 기능들을 구현했던 프로젝트입니다.
+  * 해당 프로젝트에서 벡엔드를 담당했습니다.
+  * 회원정보, 상품정보 등 정보에 대한 REST API 를 구현하려 노력했습니다.
+  * express 의 3계층 구조를 구현 구획화를 통해 유지보수성을 개선했습니다.
+  * gitFlow branch 전략을 체택하여 git 을 활용해 효율적인 협업을 수행했습니다.
+  * AWS S3 도입을 통해 static image asset 을 처리해 많은 트레픽에 대비 했습니다.
  `,
   secArticle: `
-  ## git 주소
-  * 두번째 아티클 내용입니다
-  * https://github.com/Elice-Team-20/Collectors
+  ## 사용된 기술
+  * git, HTML, CSS, JavaScript, PM2, express, AWS s3
  `,
 };
